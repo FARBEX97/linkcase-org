29cd2b1f7ae929eea59940b497e50aebb4563e20
FARBEX97 <fernando.arbex97@gmail.com>
2021-03-13 17:43:49 +0100

    upgraded

--- workspaces/forms/workspace.py
+++ workspaces/forms/workspace.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired
 
 class NewWorkspaceForm(FlaskForm):
     ws_name = StringField('New Workspace', validators=[DataRequired()], render_kw={"placeholder": "New Workspace"})
-    submit = SubmitField('Create Workspace')
+    submit = SubmitField('+')
 
 
 class DeleteWorkspaceForm(FlaskForm):
