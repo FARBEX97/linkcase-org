8c497ee0683b8d0502b13643c896e393fd9d3d58
FARBEX97 <fernando.arbex97@gmail.com>
2021-03-22 21:37:30 +0100

    upgrades

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-dev = True
+dev = False
 
 class DevConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -9,3 +9,5 @@ class DevConfig(object):
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'linkcaseorg.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
