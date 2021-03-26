b961b80099cef3ceccaa8fb95ebfdf1aea7a2932
farbex <fernando.arbex97@gmail.com>
2021-03-26 17:22:59 +0100

    upgrade UI

--- linkcases/forms/link.py
+++ linkcases/forms/link.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 
 
 class NewLinkForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()], render_kw={'placeholder': 'Url name'})
+    name = StringField('Name', validators=[DataRequired()], render_kw={'placeholder': 'Name'})
     url = StringField('Url', validators=[DataRequired()], render_kw={'placeholder': 'Url'})
     # workspace = SelectField('Workspace', coerce=str, validators=[DataRequired()])
     submit = SubmitField('Add')
